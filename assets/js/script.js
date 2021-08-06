82722337c09c53046a30eca7309fff2d235855d8
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2021-08-06 12:50:29 +0000

    Add function to select a mole hole at random

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,23 @@ const numberMiss = document.querySelector('.miss');
 const borisMole = document.querySelectorAll('.boris');
 
 const timer = document.querySelector('.time');
+
+// Start of game variables - values will change throughout game session
+
+let timeup = false;
+let timeLimit = 30000;
+let score = 0;
+let countdown;
+let lastHole;
+
+// Function to select a hole at random
+
+function selectHole(holes) {
+    let randomHole = Math.floor(Math.random() * holes.length);
+    let hole = holes[randomHole];
+    if (hole === lastHole){
+        return selectHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+}
