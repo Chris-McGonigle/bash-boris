86c156bf1087475a5a25dc8d58b0a38d59b26024
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2021-08-06 12:24:47 +0000

    Add global variables to script.js. Add HTML to count number of missed hits

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Global variables
+
+const holes = document.querySelectorAll('.mole-hole');
+const numberHits = document.querySelector('.score');
+const numberMiss = document.querySelector('.miss');
+const borisMole = document.querySelectorAll('.boris');
+
+const timer = document.querySelector('.time');
