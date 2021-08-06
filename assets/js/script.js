cfee16601f1d858b9861dcfd70acb71a85eb9106
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2021-08-06 13:24:42 +0000

    Add function to animate mole up and down

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ const timer = document.querySelector('.time');
 
 // Start of game variables - values will change throughout game session
 
-let timeup = false;
+let timeUp = false;
 let timeLimit = 30000;
 let score = 0;
 let countdown;
@@ -25,4 +25,16 @@ function selectHole(holes) {
     }
     lastHole = hole;
     return hole;
+}
+
+// Function to animate mole up and hide after random period of time until game over timer
+
+function borisUp() {
+    let time = Math.random() * 2500 + 500;
+    let chosenHole = selectHole(holes);
+    chosenHole.classList.add('peek');
+    setTimeout(function(){
+        chosenHole.classList.remove('peek');
+        if (!timeUp) borisUp();
+    }, time);        
 }
